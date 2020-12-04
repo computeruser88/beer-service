a15a1f5f2328e2bd2c1d939909a58dfb2b7e8c5d
Jan Balangue <jan.a.balangue@gmail.com>
2020-12-03 20:32:29 -0600

    Add BeerControllerTest.java and fix error in BeerController.java.

--- src/main/java/io/github/computeruser88/beerservice/web/controller/BeerController.java
+++ src/main/java/io/github/computeruser88/beerservice/web/controller/BeerController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @RestController
 public class BeerController {
 
+    @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
 
         // todo impl
