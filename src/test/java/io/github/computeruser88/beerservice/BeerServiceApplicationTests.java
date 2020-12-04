2423670a6c24b2f01e01173477cfc0e1e394b8fa
Jan Balangue <jan.a.balangue@gmail.com>
2020-12-03 19:34:19 -0600

    Initial project add.

---
+++ src/test/java/io/github/computeruser88/beerservice/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.computeruser88.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
